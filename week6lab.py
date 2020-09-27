a46740afb25528e44228825e047d7cb8dca0f3eb
Daniel Lucash <spelcaster0921@gmail.com>
2020-09-27 13:17:50 -0400

    Update week6lab.py

--- week6lab.py
+++ week6lab.py
@@ -1,6 +1,9 @@
+Dan Lucash, Lab C
+Nathan Barta, Lab A
 name = input("What is your name? ")
 age = int(input("How old are you? "))
 
 #seconds =
+seconds = int(age*31536000)
 
 print(name + " has lived for at least " + seconds + " seconds!")
